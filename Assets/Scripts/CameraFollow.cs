4f170dd2f785310a89f8596d96f3eb709f78995e
Saher <saher.ziauddin@dal.ca>
2020-12-23 23:13:15 +0300

    added a feature where camera follows the ball

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 endPosition = player.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, endPosition, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
