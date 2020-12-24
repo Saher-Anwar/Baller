4e3482d3480f2e550b4f080ea7ef98319da6808b
Saher <saher.ziauddin@dal.ca>
2020-12-24 17:37:25 +0300

    added camera zoom in and out effect

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,6 +10,17 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     private bool wasBig = false;
 
+    private Camera cam;
+    private float zoomOutFactor = 3f;
+    private float targetZoom;
+    private float zoomLerpSpeed = 10f;
+
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+    }
+
     private void FixedUpdate()
     {
         bool isXPositivelyFaster = player.GetComponent<Rigidbody2D>().velocity.x > 10;
@@ -17,6 +28,17 @@ public class CameraFollow : MonoBehaviour
         bool isYPositivelyFaster = player.GetComponent<Rigidbody2D>().velocity.y > 10;
         bool isYNegativelyFaster = player.GetComponent<Rigidbody2D>().velocity.y < -10;
 
+
+        if (isXPositivelyFaster || isXNegativelyFaster || isYPositivelyFaster || isYNegativelyFaster)
+        {
+            targetZoom = 8;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+        }
+        else
+        {
+            targetZoom = 5;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+        }
 
         Vector3 endPosition = player.transform.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, endPosition, smoothSpeed);
